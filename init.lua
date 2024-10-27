945e8273e03ee678597a4e5958625a719458044f
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-10-27 11:08:30 +0100

    add sensible defaults for tab

--- init.lua
+++ init.lua
@@ -102,7 +102,13 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
+
+-- Set sensible defaults for tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
